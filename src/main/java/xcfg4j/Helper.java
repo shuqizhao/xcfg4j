07395f41cc3f4974e06455db3b1cf846d69f4dce
shuqizhao <shuqizhao@beisen.com>
2018-04-01 15:56:27 +0800

    done

--- src/main/java/xcfg4j/Helper.java
+++ src/main/java/xcfg4j/Helper.java
@@ -46,7 +46,7 @@ public class Helper {
 	
 	private static String getRuningPath() {
 //		return Helper.class.getResource("").getPath();
-		File directory = new File("");//设定为当前文件夹 
+		File directory = new File(".");//设定为当前文件夹 
 		try{ 
 //		    System.out.println(directory.getCanonicalPath());//获取标准的路径 
 //		    System.out.println(directory.getAbsolutePath());//获取绝对路径 
